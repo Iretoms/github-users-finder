aec2fdaa0bb99f32247ff432634f636578f30eb1
iretoms <iretomiwajonathan@gmail.com>
2022-06-01 03:08:53 -0700

    fourth commit

--- script.js
+++ script.js
@@ -9,55 +9,72 @@ function fetchUser(name){
         .then(response => {
             showUser(response.data);
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+            if( error.response.status === 404){
+                createErrorCard('This user doesnt exist')
+            }
+        });
 };
 
+
+
+function createErrorCard(message){
+    let person = document.createElement('div')
+    person.classList.add('user')
+    person.innerHTML = `<h2>${message}</h2>`
+    container.appendChild(person)
+}
+
+
 function showUser(user){
+    let person = document.createElement('div')
+    person.classList.add('user')
+    
     const {avatar_url,public_repos, following, followers, bio, name,login,html_url,created_at} = user
-    let user = document.createElement('div')
-    user.classList.add('user')
-    user.innerHTML = `
+    let date = new Date(created_at).toDateString()
+    person.innerHTML = `
     <div class="user-image">
-    <img src="" alt="">
+    <img src="${avatar_url}" alt="${name}">
     </div>
 
     <div class="user-info">
     <div class="name">
         <div>
-            <h2>The Octocat</h2>
-            <small>@octocat</small>
+            <h2>${name}</h2>
+            <small>@${login}</small>
         </div>
         <small>
-            Joined Tue Jan 25 2011
+            Joined ${date}
         </small>
     </div>
 
     <div class="about">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-         Facilis doloremque totam deleniti? Nostrum, sapiente
-          aperiam.
+        ${bio ? bio : ''}
     </div>
 
     <div class="activity">
         <div class="repos">
             <small>Repos</small>
-            <h2>8</h2>
+            <h2>${public_repos}</h2>
         </div>
         <div class="followers">
             <small>Followers</small>
-            <h2>5650</h2>
+            <h2>${followers}</h2>
         </div>
         <div class="following">
             <small>Following</small>
-            <h2>9</h2>
+            <h2>${following}</h2>
         </div>
     </div>
 
     <div class="link">
         <i class="fa-solid fa-link"></i>
-        <a href="#">https://github.com</a>
+        <a href="${html_url}">${html_url}</a>
     </div>
    </div>`
+
+   container.appendChild(person);
+   
 }
 
 
@@ -65,11 +82,11 @@ function showUser(user){
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
 
-    const username =search.value
+    const username = search.value
 
-    if(username && username !== ''){
+    if(username){
         fetchUser(username)
+
+        search.value = ''
     }
-
-
 })
